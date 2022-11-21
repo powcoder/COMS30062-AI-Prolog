56f313ec0dc36322aabcb86524d1da946b203557
powcoder <powcoder@163.com>
2022-11-21 22:06:58 +0800

    COMS30062 AI Prolog

---
+++ cw_12345.pl
@@ -0,0 +1,22 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+% Accomplish a given Task and return the Cost
+solve_task(Task,Cost) :-
+    my_agent(A), get_agent_position(A,P),
+    solve_task_dfs(Task,[P],[P|Path]), !,
+    agent_do_moves(A,Path), length(Path,Cost).
+
+% Calculate the path required to achieve a Task
+solve_task_dfs(Task,[P|Ps],Path) :-
+    achieved(Task,P), reverse([P|Ps],Path)
+    ;
+    map_adjacent(P,Q,empty), \+ member(Q,Ps),
+    solve_task_dfs(Task,[Q,P|Ps],Path).
+
+% True if the Task is achieved with the agent at Pos
+achieved(Task,Pos) :- 
+    Task=find(Obj), map_adjacent(Pos,_,Obj)
+    ;
+    Task=go(Pos).
---
+++ cw_maze_12345.pl
@@ -0,0 +1,20 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+% Solve the maze, aiming to get all the agents to p(N,N)
+solve_maze :-
+    my_agents(Agents),
+    find_moves(Agents,Moves),
+    agents_do_moves(Agents,Moves),
+    solve_maze.
+    
+
+%%%%%%%%%%%%%%%% USEFUL PREDICATES %%%%%%%%%%%%%%%%%%
+% Find a possible move for each agent
+find_moves([],[]).
+find_moves([A|As],[M|Moves]) :-
+    findall(P,agent_adjacent(A,P,_),PosMoves),
+    random_member(M,PosMoves),
+    find_moves(As,Moves).
+
